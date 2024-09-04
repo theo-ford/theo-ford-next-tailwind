822bd690ff8c61f965f00c5260ed996a952ba78c
Theo Ford <theoford@Theos-MBP.cust.communityfibre.co.uk>
2024-09-04 16:32:04 +0100

    20240904 16:31 added the nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import * as prismic from "@prismicio/client";
 import { createClient } from "@/prismicio";
 import { components } from "@/slices";
 import { PrismicNextLink } from "@prismicio/next";
+import Link from "next/link";
 import { PrismicRichText } from "@prismicio/react";
 import Logo from "@/app/components/Logo";
 
@@ -33,12 +34,25 @@ export default async function Index() {
 
   return (
     <div>
-      <div className="bg-white w-full h-screen">
-        <div className="mix-blend-exclusion w-25 sticky top-[12.5px] ml-[12.5px]">
-          <Logo />
+      <div className="bg-white">
+        <nav className="sticky ml-[12.5px] z-30 mix-blend-exclusion top-[12.5px]">
+          <div className="mix-blend-exclusion w-25 sticky top-[12.5px] ml-[12.5px] relative float-left">
+            <Logo />
+          </div>
+          <div className="ml-[50%] w-full relative mix-blend-exclusion">
+            <p className="mix-blend-exclusion font-sans leading-none text-[#878787] relative float-left">
+              <Link href="/">Selected,</Link>&nbsp;
+              <Link href="/">Index,</Link>&nbsp;
+              <Link href="/">Office</Link>
+            </p>
+          </div>
+        </nav>
+        <div>
+          <div className="h-screen w-full bg-red-800"></div>
+          <div className="h-screen w-full bg-green-800">
+            <ol className="text-lg">{projects}</ol>
+          </div>
         </div>
-        <ol className="text-lg mt-[500px]">{projects}</ol>
-        <div class="h-screen w-full bg-red-800"></div>
       </div>
 
       {/* <SliceZone slices={home.data.slices} components={components} /> */}
--- src/app/project/[uid]/page.tsx
+++ src/app/project/[uid]/page.tsx
@@ -1,12 +1,11 @@
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
-
 import { SliceZone, PrismicRichText } from "@prismicio/react";
 import * as prismic from "@prismicio/client";
-
 import { createClient } from "@/prismicio";
 import { components } from "@/slices";
-
+import { PrismicNextLink } from "@prismicio/next";
+import Link from "next/link";
 import Logo from "@/app/components/Logo";
 
 type Params = { uid: string };
@@ -45,24 +44,34 @@ export default async function Page({ params }: { params: Params }) {
       <div
         className={`${page.data.hero_color === "White" ? "bg-white" : "bg-black"}`}
       >
-        <div className="sticky ml-[12.5px] z-40 mix-blend-exclusion bg-red-300 top-[12.5px]">
-          <div className="">
-            <div className="mix-blend-exclusion animate-scale w-full relative float-left mt-[2px]">
-              <Logo />
-            </div>
-            <p className="mix-blend-exclusion font-sans leading-none text-white relative float-left">
-              <span className="animate-fade opacity-[0]">
-                &nbsp; for {page.data.title} in {page.data.location}.{" "}
-                {page.data.year}.
-              </span>
+        <nav className="sticky ml-[12.5px] z-30 mix-blend-exclusion top-[12.5px]">
+          <div className="mix-blend-exclusion animate-scale w-full relative float-left mt-[2px]">
+            <Logo />
+          </div>
+          <p className="mix-blend-exclusion font-sans leading-none text-white relative float-left">
+            <span className="animate-fade opacity-[0]">
+              &nbsp; for {page.data.title} in {page.data.location}.{" "}
+              {page.data.year}.
+            </span>
+          </p>
+          <div className="ml-[50%] w-full relative  mix-blend-exclusion animate-fade opacity-[0]">
+            <p className="mix-blend-exclusion font-sans leading-none text-[#878787] relative float-left">
+              <Link href="/">Selected,</Link>&nbsp;
+              <Link href="/">Index,</Link>&nbsp;
+              <Link href="/">Office</Link>
             </p>
           </div>
-        </div>
+        </nav>
 
-        <div
-          className={`w-full h-screen ${page.data.hero_color === "White" ? "bg-white" : "bg-black"} animate-fade opacity-[0]"`}
-        ></div>
-        <SliceZone slices={page.data.slices} components={components} />
+        <div className="">
+          <div
+            className={`w-full h-screen ${page.data.hero_color === "White" ? "bg-white" : "bg-black"} animate-fade opacity-[0]"`}
+          ></div>
+
+          <div className="h-screen w-full bg-red-800">
+            <SliceZone slices={page.data.slices} components={components} />
+          </div>
+        </div>
       </div>
     </>
   );
